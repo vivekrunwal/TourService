4a02cd2f2f74666bc58c4199f25b0e9f081af77d
Vivek Runwal <vivekrunwal7@gmail.com>
2022-06-24 12:04:03 +0530

    Paging and Sorting

--- src/main/java/com/tour/tourservice/repo/TourRepository.java
+++ src/main/java/com/tour/tourservice/repo/TourRepository.java
@@ -2,15 +2,55 @@ package com.tour.tourservice.repo;
 
 import com.tour.tourservice.Domain.Tour;
 import com.tour.tourservice.Domain.TourPackage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
-@Repository
-public interface TourRepository extends CrudRepository<Tour,Integer> {
+
+public interface TourRepository extends PagingAndSortingRepository<Tour,Integer> {
 
 
+    Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
+
+    @Override
+    @RestResource(exported = false)
+    Iterable<Tour> findAll(Sort sort);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllById(Iterable<? extends Integer> integers);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 //    Optional<Tour> findByName(Integer id);
 
 }
